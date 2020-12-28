9d5c859707fa7d342a42abcfa1b94e3e1b83b182
gulshanyadav01 <gulshany01@gmail.com>
2020-12-28 09:22:45 +0530

    db is connected

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose"); 
+
+const config  = require("config"); 
+const { builtinModules } = require("module");
+
+const db = config.get("mongoURI"); 
+
+const connectDB = async () =>{
+    try{
+       await  mongoose.connect(db, {
+           useNewUrlParser: true,
+           useUnifiedTopology: true
+       }); 
+       console.log("MONGODB CONNECTED");
+
+    }catch(err){
+        console.log(err.message); 
+        // exit process with fail 
+        process.exit(); 
+    }
+}
+
+module.exports = connectDB; 
