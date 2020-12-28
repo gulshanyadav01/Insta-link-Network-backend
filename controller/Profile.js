1fab00b651d067d1db6732ea0d65dab10df08d2e
gulshanyadav01 <gulshany01@gmail.com>
2020-12-28 15:32:03 +0530

    work is in progress in profile routes

--- controller/Profile.js
+++ controller/Profile.js
@@ -1,5 +1,17 @@
+const Profile = require("../model/Profile")
+const User = require("../model/User")
 
+exports.getProfileMe = async (req, res, next) =>{
+    try{
+        const profile = await Profile.findOne({user: req.user.id}).populate("user", ["name", 'avatar']);
+        if(!profile){
+            return res.status(400).json({msg:"there is no profile for this user"});
 
-exports.getProfileMe = (req, res, next) =>{
-    res.send("hi"); 
+        }
+        res.json(profile);
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("server error"); 
+    }
 }
